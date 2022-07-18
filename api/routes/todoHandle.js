8c9ddca6de2793ba2f1216012e1547ade7cdb3a3
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2022-07-18 18:59:44 +0600

    todo put

--- api/routes/todoHandle.js
+++ api/routes/todoHandle.js
@@ -6,15 +6,7 @@ const todoSchema = require('../models/todoSchema')
 const Todo = new mongoose.model("Todo", todoSchema)
 
 
-// get all the todo
-router.get('/', async (req, res) => {
 
-})
-
-// get a todo by id
-router.get('/:id', async (req, res) => {
-
-})
 
 // post a todo
 router.post('/', async (req, res) => {
@@ -54,10 +46,56 @@ router.post('/all', async (req, res) => {
 })
 
 // put  todo
-router.put('/:id', async (req, res) => {
+// router.put('/:id', async (req, res) => {
+//     const id = req.params.id
+//     await Todo.updateOne({_id:id},{
+//         $set:{
+//             status:'active'
+//         }
+//     },(err)=>{
+//         if (err) {
+//             res.status(500).json({
+//                 error: "there are a server side error."
+//             })
+//         } else {
+//             res.status(201).json({
+//                 message: "todo was updated successfully."
+//             })
+//         }
+//     })
+// })
 
+router.put('/:id', async (req, res) => {
+    const id = req.params.id
+    await Todo.findByIdAndUpdate({ _id: id }, {
+        $set: {
+            status: 'active'
+        }
+    }, {
+        new: true,
+        useFindAndModify: false
+    }
+        , (err, data) => {
+            if (err) {
+                res.status(500).json({
+                    error: "there are a server side error."
+                })
+            } else {
+                res.status(200).json({
+                    message: "todo was updated successfully.",
+                    data: data
+                })
+            }
+        })
 })
 
+
+// multiple filter
+router.put('/:id', async (req, res) => {
+    const id = req.params.id
+    const status = req.params.status
+    await Todo.updateOne({ _id: id, status: status })
+})
 
 // delete todo
 router.delete('/:id', async (req, res) => {
